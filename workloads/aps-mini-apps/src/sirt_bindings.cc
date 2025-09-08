fb3032cfa7b3e875c12b9658bd20a166bdac85d9
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-09-08 10:36:49 -0500

    fix mpi inclusion and data_stream

--- workloads/aps-mini-apps/src/sirt_bindings.cc
+++ workloads/aps-mini-apps/src/sirt_bindings.cc
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
-#include "sirt.h"
+#include "sirt_engine.h"
 #include <cstdint>
 #include <cstring>
 #include <stdexcept>
