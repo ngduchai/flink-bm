43338a3717c2579ae644d6073e29e101b7e52fbe
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-09-30 10:51:22 -0500

    test checlpointing

--- workloads/aps-mini-apps/execute-pipeline.py
+++ workloads/aps-mini-apps/execute-pipeline.py
@@ -647,8 +647,8 @@ def main():
     cfg.set_string("execution.checkpointing.dir", ckpt_dir)
     cfg.set_string("execution.checkpointing.savepoint-dir", ckpt_dir)
 
-    cfg.set_integer("execution.checkpointing.timeout", 1000000)  # 1 minutes
-    cfg.set_integer("execution.checkpointing.min-pause", 1000000)  # 5 seconds between checkpoints
+    cfg.set_integer("execution.checkpointing.timeout", 60000)  # 1 minutes
+    cfg.set_integer("execution.checkpointing.min-pause", 120000)  # 5 seconds between checkpoints
     cfg.set_string("akka.ask.timeout", "60s")
 
     env = StreamExecutionEnvironment.get_execution_environment(cfg)
