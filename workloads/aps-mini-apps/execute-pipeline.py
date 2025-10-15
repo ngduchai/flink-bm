f6b1b39210cc1068da6c369324d3a270e0653b5d
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-15 02:32:12 -0500

    update denoise for multiple sirt exp

--- workloads/aps-mini-apps/execute-pipeline.py
+++ workloads/aps-mini-apps/execute-pipeline.py
@@ -836,8 +836,10 @@ def main():
     # ck.set_checkpoint_timeout(15 * 60 * 1000)          # 15 min timeout
     # ck.set_max_concurrent_checkpoints(1)               # avoid overlaps
     # ck.set_min_pause_between_checkpoints(5 * 1000)     # 5s pause
-    ck.enable_unaligned_checkpoints(True)              # helps under backpressure
-    ck.set_aligned_checkpoint_timeout(Duration.of_seconds(0))        # switch to unaligned if align >3s
+
+    # Comment out because we use custom partitioning
+    # ck.enable_unaligned_checkpoints(True)              # helps under backpressure
+    # ck.set_aligned_checkpoint_timeout(Duration.of_seconds(0))        # switch to unaligned if align >3s
 
     # env.disable_operator_chaining()
     # env.set_buffer_timeout(100)
