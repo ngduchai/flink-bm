c0ad4b2e69f0831082428c0c2ae7cc054673a610
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-15 02:35:39 -0500

    update denoise for multiple sirt exp

--- workloads/aps-mini-apps/execute-pipeline.py
+++ workloads/aps-mini-apps/execute-pipeline.py
@@ -815,7 +815,7 @@ def main():
     cfg.set_integer("state.backend.rocksdb.max-write-buffer-number", 4)
     cfg.set_string("execution.checkpointing.dir", ckpt_dir)
     cfg.set_string("execution.checkpointing.savepoint-dir", ckpt_dir)
-    cfg.set_boolean("execution.checkpointing.unaligned.enabled", True)
+    # cfg.set_boolean("execution.checkpointing.unaligned.enabled", True)
 
     cfg.set_integer("execution.checkpointing.timeout", 60000)  # 1 minutes
     cfg.set_string("akka.ask.timeout", "60s")
