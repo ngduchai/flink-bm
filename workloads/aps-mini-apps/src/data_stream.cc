4fa2955c2d920972ff1aa7dce56e8eaf079a3e88
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-24 17:37:30 -0500

    add seq checkpoint to daq

--- workloads/aps-mini-apps/src/data_stream.cc
+++ workloads/aps-mini-apps/src/data_stream.cc
@@ -81,10 +81,10 @@ DataRegionBase<float, TraceMetadata>* DataStream::setupTraceDataRegion(
       mdata);
   curr_data->ResetMirroredRegionIter();
   
-  auto p = reinterpret_cast<const unsigned char*>(data);
-  std::cout << "SirtReconCheck: "
-    << "First float value: " << data[0]
-    << " First value: " << static_cast<unsigned>(p[0]) << std::endl;
+  // auto p = reinterpret_cast<const unsigned char*>(data);
+  // std::cout << "SirtReconCheck: "
+  //   << "First float value: " << data[0]
+  //   << " First value: " << static_cast<unsigned>(p[0]) << std::endl;
 
   return curr_data;
 }
