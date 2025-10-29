91809d6c2d8839448d7202714a7865b156771fa6
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-28 22:33:29 -0500

    check recosntruction after scaling

--- workloads/aps-mini-apps/setup.py
+++ workloads/aps-mini-apps/setup.py
@@ -5,10 +5,10 @@ import sys, pybind11, os
 # TODO: Adjust these paths as necessary
 # here = "/home/ndhai/diaspora/flink-bm/workloads/aps-mini-apps/"
 # localview = "/home/ndhai/diaspora/spack-aps/var/spack/environments/APS/.spack-env/view/"
-here = "/home/ndhai/src/flink-bm/workloads/aps-mini-apps"
-localview = "/home/ndhai/src/spack-aps/var/spack/environments/APS/.spack-env/view/"
-# here = "/opt/workloads/aps-mini-apps"
+# here = "/home/ndhai/src/flink-bm/workloads/aps-mini-apps"
 # localview = "/home/ndhai/src/spack-aps/var/spack/environments/APS/.spack-env/view/"
+here = "/opt/workloads/aps-mini-apps"
+localview = "/home/ndhai/src/spack-aps/var/spack/environments/APS/.spack-env/view/"
 locallib = localview + "lib"
 localinclude = localview + "include"
 
