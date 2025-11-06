74764f3ce682ca59efc3c072959d47ef0fe0c46f
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-11-06 21:24:11 +0000

    convert recon processors to pointer instead of value

--- workloads/aps-mini-apps/execute-pipeline.py
+++ workloads/aps-mini-apps/execute-pipeline.py
@@ -1001,28 +1001,28 @@ class SirtOperator(FlatMapFunction):
         if self._restored:
             return
         try:
-            # raw = self.snap_state.value()   # keyed ValueState for the current key
-            # cnt_state = self.count_state.value()
-            # # print(f"[SirtOperator]: restoring from checkpoint: count = {cnt_state}")
-            # # print(f"[SirtOperator]: restoring from checkpoint: self = {len(raw)}")
-            # if raw:
-            #     raw_bytes = raw if isinstance(raw, (bytes, bytearray)) else bytes(raw)
-            #     print(f"[SirtOperator]: found previous state: {len(raw_bytes)} bytes. Restoring")
-            #     import sirt_ops
-            #     with sirt_ops.ostream_redirect(): 
-            #         self.engine.restore(raw_bytes)
-            #     # also restore counter if present
-            #     # cnt = self.count_state.value()
-            #     # cnt = cnt_state
-            #     self.processed_local = int(cnt_state) if cnt_state is not None else self.processed_local
-            #     print(f"[SirtOperator] restored {len(raw_bytes)} bytes from state with processed_local = {cnt_state}")
-            # else:
-            #     # No bytes yet for this key; don't flip the flag so we can retry
-            #     print(f"[SirtOperator] cannot find previous state. Start from beginning")
-            #     return
+            raw = self.snap_state.value()   # keyed ValueState for the current key
             cnt_state = self.count_state.value()
-            self.processed_local = int(cnt_state) if cnt_state is not None else self.processed_local
-            print(f"[SirtOperator] restored with processed_local = {self.processed_local}")
+            # print(f"[SirtOperator]: restoring from checkpoint: count = {cnt_state}")
+            # print(f"[SirtOperator]: restoring from checkpoint: self = {len(raw)}")
+            if raw:
+                raw_bytes = raw if isinstance(raw, (bytes, bytearray)) else bytes(raw)
+                print(f"[SirtOperator]: found previous state: {len(raw_bytes)} bytes. Restoring")
+                import sirt_ops
+                with sirt_ops.ostream_redirect(): 
+                    self.engine.restore(raw_bytes)
+                # also restore counter if present
+                # cnt = self.count_state.value()
+                # cnt = cnt_state
+                self.processed_local = int(cnt_state) if cnt_state is not None else self.processed_local
+                print(f"[SirtOperator] Restored {len(raw_bytes)} bytes from state with processed_local = {cnt_state}")
+            else:
+                # No bytes yet for this key; don't flip the flag so we can retry
+                print(f"[SirtOperator] Cannot find previous state. Start from beginning")
+                return
+            # cnt_state = self.count_state.value()
+            # self.processed_local = int(cnt_state) if cnt_state is not None else self.processed_local
+            # print(f"[SirtOperator] restored with processed_local = {self.processed_local}")
         except Exception as e:
             print("[SirtOperator] restore step failed:", e, file=sys.stderr)
             traceback.print_exc()
@@ -1037,7 +1037,7 @@ class SirtOperator(FlatMapFunction):
             with sirt_ops.ostream_redirect(): 
                 snap = self.engine.snapshot()
             snap_bytes = snap if isinstance(snap, (bytes, bytearray)) else bytes(snap)
-            # self.snap_state.update(snap_bytes)
+            self.snap_state.update(snap_bytes)
             # self.snap_state.update(bytes([1, 2, 3]))
             self.count_state.update(self.processed_local)
             # raw = self.snap_state.value()
